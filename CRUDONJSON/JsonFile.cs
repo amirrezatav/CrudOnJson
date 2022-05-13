0b03d32c2386fecc22f93e3819905a1f5729ad11
Amirreza Tavakoli <amirrezatav2@gmail.com>
2022-05-14 00:16:36 +0430

    Add project files.

---
+++ CRUDONJSON/JsonFile.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDONJSON
+{
+    public class JsonFile
+    {
+        private readonly string path; // Directory of storage
+        // path is Directory not file !!
+        public JsonFile(string root) // root is Directory 
+        {
+            path = root;
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        public void Create<T> (T obj)
+        {
+            List<T> NewItems = new List<T>();
+            NewItems.Add(obj);
+
+            Type type = typeof (T); // get class information
+            string className = type.Name; // get class name
+            string filename = className + ".json";
+            // path -> c:/Directory1/Directory2/Directory3
+            // filename -> babaei.json
+            // filepath -> c:/Directory1/Directory2/Directory3/filename
+            //             c:/Directory1/Directory2/Directory3/babaei.json
+            string filepath = Path.Combine(path, filename);
+            
+            if(!File.Exists(filepath))
+            { // file is not exist
+                // Convert Object To json
+                string ObjectInJson = JsonConvert.SerializeObject(NewItems , Formatting.Indented);
+                File.WriteAllText(filepath, ObjectInJson);
+            }
+            else
+            { // File is exist
+
+                string ObjectsJson = File.ReadAllText(filepath);
+
+                List<T> ExistItem = JsonConvert.DeserializeObject<List<T>>(ObjectsJson);
+
+                if(ExistItem != null)
+                    NewItems.AddRange(ExistItem);
+
+                string ObjectInJson = JsonConvert.SerializeObject(NewItems, Formatting.Indented);
+
+                File.WriteAllText(filepath, ObjectInJson);
+            }
+        }
+        public List<T> Read<T>()
+        {
+            Type type = typeof(T);
+            string className = type.Name;
+            string filename = className + ".json";
+            string filepath = Path.Combine(path, filename);
+
+            if (File.Exists(filepath))
+            {
+                string ObjectsJson = File.ReadAllText(filepath);
+
+                List<T> ExistItem = JsonConvert.DeserializeObject<List<T>>(ObjectsJson);
+
+                return ExistItem;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Update<T>(T obj , Predicate<T> pre)
+        {
+            Type type = typeof(T);
+            string className = type.Name;
+            string filename = className + ".json";
+            string filepath = Path.Combine(path, filename);
+
+            if (File.Exists(filepath))
+            {
+                string ObjectsJson = File.ReadAllText(filepath);
+
+                List<T> ExistItem = JsonConvert.DeserializeObject<List<T>>(ObjectsJson);
+
+                ExistItem.RemoveAll(pre);
+
+                ExistItem.Add(obj);
+
+                string ObjectInJson = JsonConvert.SerializeObject(ExistItem, Formatting.Indented);
+
+                File.WriteAllText(filepath, ObjectInJson);
+            }
+        }
+        public void Delete<T>(Predicate<T> pre)
+        {
+            Type type = typeof(T); 
+            string className = type.Name; 
+            string filename = className + ".json";
+            string filepath = Path.Combine(path, filename);
+
+            if (File.Exists(filepath))
+            {
+                string ObjectsJson = File.ReadAllText(filepath);
+
+                List<T> ExistItem = JsonConvert.DeserializeObject<List<T>>(ObjectsJson);
+
+                ExistItem.RemoveAll(pre);
+
+                string ObjectInJson = JsonConvert.SerializeObject(ExistItem, Formatting.Indented);
+
+                File.WriteAllText(filepath, ObjectInJson);
+            }
+        }
+    }
+}
---
+++ CRUDONJSON/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDONJSON
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // string root = Directory.GetCurrentDirectory();
+            string root = "."; // exe directory 
+            JsonFile jsonFile = new JsonFile(root);
+
+            Sheep babaei = new Sheep()
+            {
+                Id = 1,
+                Name = "sab",
+                Age = 3,
+            };
+            jsonFile.Create<Sheep>(babaei);
+
+            babaei = new Sheep()
+            {
+                Id = 2,
+                Name = "askhar",
+                Age = 9,
+            };
+            jsonFile.Create<Sheep>(babaei);
+            babaei = new Sheep()
+            {
+                Id = 3,
+                Name = "has",
+                Age = 8,
+            };
+            jsonFile.Create(babaei);
+            babaei = new Sheep()
+            {
+                Id = 4,
+                Name = "ham",
+                Age = 7,
+            };
+            jsonFile.Create(babaei);
+            babaei = new Sheep()
+            {
+                Id = 5,
+                Name = "bab",
+                Age = 6,
+            };
+            jsonFile.Create(babaei);
+
+
+
+            jsonFile.Delete<Sheep>(x => x.Id == 5);
+
+            babaei = new Sheep()
+            {
+                Id = 5,
+                Name = "sal",
+                Age = 6,
+            };
+
+            jsonFile.Update(babaei , x => x.Id == 5);
+
+        }
+    }
+}
